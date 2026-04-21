28a3c6cdc7339d2bef5473af25a269d698348c69
yargames1 <89339132+yargames1@users.noreply.github.com>
2026-04-21 18:51:00 +0300

    r1 полностью работает

--- gitScripts/test.sh
+++ gitScripts/test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 DIR="repo"
-bash init.sh "$DIR"
+bash init.sh "$DIR" # r0
 echo "начинаем делать ветку"
 ls
-bash new-branch.sh test3 r1 "$DIR"
+bash new-branch.sh test3 r1 "$DIR" # r1
+ls
+# r2
+
+# r3
+
+# r4
+
+
 
 read -p "Press Enter to exit..."
