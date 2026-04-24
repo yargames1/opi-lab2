87ce4d5987f34c669262931e145cdda69be5d334
TheGeniusOfEternity <max06safin@yandex.ru>
2026-04-24 13:16:34 +0300

    feat(git): add second user commits

--- git-scripts/set-user.sh
+++ git-scripts/set-user.sh
@@ -6,4 +6,6 @@ repository="${2:-.}"
 git -C "$repository" config user.name "${user}"
 git -C "$repository" config user.email "${user}@mail.com"
 
+printf "\n"
 echo "User ${user} set as active"
+printf "\n"
