98ab410304c7563563fc4e72f8dfa0d129a19fe4
TheGeniusOfEternity <max06safin@yandex.ru>
2026-04-24 14:37:09 +0300

    feat(git): add merge commits

--- git-scripts/commit.sh
+++ git-scripts/commit.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # params
-commitNum="${1:-none}"
+commitNum="${1:-r0}"
 DIR="${2:-repo}"
 
 # move to repo's dir
 cd "$DIR"|| exit 1
 
 # make changes
-if [ "$commitNum" != "none" ]; then
   echo "Making some changes"
   cp -r ../../output/"${commitNum}"/* .
   git add .
+
+# check unresolved conflicts
+if git ls-files -u | grep .; then
+  echo "ERROR: Unresolved conflicts found!"
+  git status
+  exit 1
 fi
 
 # commit changes
--- git-scripts/merge.sh
+++ git-scripts/merge.sh
@@ -8,14 +8,14 @@ isForce="${3:-1}"
 # move to repo's dir
 cd "$DIR"|| exit 1
 
-# force merge with ours changes
 if [ "$isForce" -eq 0 ]; then
+  # force merge with ours changes
   git merge "${featureBranch}" -s ours --no-commit
-fi
 
-# resolve conflicts manually
-if ! git merge "${featureBranch}" --no-commit; then
-  echo "Merge conflict detected."
-  echo "Please resolve conflicts, then press Enter."
-  read -r
+else
+  # resolve conflicts manually
+  if ! git merge "${featureBranch}" --no-commit; then
+    echo "Please resolve conflicts, then press Enter."
+    read -r
+  fi
 fi
